d565ab05699f897320bd5876c2a5e504ad649ef3
fkrj021 <rm92275@estudante.fieb.edu.br>
2024-02-19 22:12:15 -0300

    subindo projeto

---
+++ src/main/java/com/itb/mif3an/academicologinmif3an/AcademicoLoginMif3anApplication.java
@@ -0,0 +1,13 @@
+package com.itb.mif3an.academicologinmif3an;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoLoginMif3anApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicoLoginMif3anApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/mif3an/academicologinmif3an/AcademicoLoginMif3anApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.mif3an.academicologinmif3an;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicoLoginMif3anApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

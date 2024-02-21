eb200fba796621bcfc831b4bbba92895885d4a61
fkrj021 <rm92275@estudante.fieb.edu.br>
2024-02-20 22:48:33 -0300

    enviando

---
+++ src/main/java/com/itb/mif3an/academicologinmif3an/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.itb.mif3an.academicologinmif3an.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+// @RestController Controlador para API
+
+@Controller // Controlador para appWeb
+@RequestMapping("/academico")
+public class UserController {
+	
+	@GetMapping("/login")
+	public String login() {
+		
+		return "login";
+	}
+	
+}
